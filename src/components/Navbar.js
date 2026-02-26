202cb1401d54aebd19f9d527d12944718756104b
Ahmad Mohsin <business.ahmadmohsin@gmail.com>
2026-02-26 23:17:04 +0500

    added mobile responsiveness

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,22 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
       <div className="navbar-brand">Al Mushtaraka</div>
-      <div className="navbar-links">
-        <Link to="/">Home</Link>
-        <Link to="/products">Products</Link>
-        <Link to="/agreement">Agreement</Link>
+      <div className={`navbar-links ${menuOpen ? 'open' : ''}`}>
+        <Link to="/" onClick={() => setMenuOpen(false)}>Home</Link>
+        <Link to="/products" onClick={() => setMenuOpen(false)}>Products</Link>
+        <Link to="/agreement" onClick={() => setMenuOpen(false)}>Agreement</Link>
+      </div>
+      <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+        <span></span>
+        <span></span>
+        <span></span>
       </div>
     </nav>
   );
