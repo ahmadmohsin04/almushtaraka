3f73a5cb1bbf213b754b25c0a7df6bb0edf753ce
Ahmad Mohsin <business.ahmadmohsin@gmail.com>
2026-03-18 16:10:28 +0500

    display

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import Products from './pages/Products';
+// import Home from './pages/Home';
+// import Products from './pages/Products';
 import Agreement from './pages/Agreement';
 import Admin from './pages/Admin';
-import Contact from './pages/Contact';
+// import Contact from './pages/Contact';
 import Customers from './pages/Customers';
 import SignAgreement from './pages/SignAgreement';
 import { useLocation } from 'react-router-dom';
@@ -19,12 +19,12 @@ function Layout() {
       {!hideNavbar && <Navbar />}
       <div className={hideNavbar ? '' : 'content'}>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/products" element={<Products />} />
+          {/* <Route path="/" element={<Home />} /> */}
+          {/* <Route path="/products" element={<Products />} /> */}
           <Route path="/agreement" element={<Agreement />} />
           <Route path="/admin" element={<Admin />} />
           <Route path="/customers" element={<Customers />} />
-          <Route path="/contact" element={<Contact />} />
+          {/* <Route path="/contact" element={<Contact />} /> */}
           <Route path="/sign/:token" element={<SignAgreement />} />
         </Routes>
       </div>
